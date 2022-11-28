6b770bf5c9a833a2eae63beb584a97e11518c3e2
navjot0 <rohitofficial2001@gmail.com>
2022-11-28 18:42:22 +0530

    new deployment fixes

--- server.js
+++ server.js
@@ -67,5 +67,5 @@ io.on('connection', (socket)=>{
 });
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 server.listen(PORT,()=> console.log(`Listening on port ${PORT}`));
